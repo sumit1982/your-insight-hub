f0fada9ed9a7169ada28c7f059bb90d1fc83d718
Sumit Roopchandani <sumitroopchandani@gmail.com>
2025-08-13 16:55:14 +1000

    updated details

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
+  base: "/your-insight-hub"
 }));
