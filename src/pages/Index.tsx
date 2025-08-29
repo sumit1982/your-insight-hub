fd2958568169971e6770139ff88487d621536750
Sumit Roopchandani <sumitroopchandani@gmail.com>
2025-08-29 12:51:47 +1000

    updated Vapi bot size

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,24 +5,39 @@ import UseCases from "@/components/UseCases";
 import About from "@/components/About";
 
 const Index = () => {
+  console.log("🚀 Index component is rendering");
+
   return (
     <div className="min-h-screen">
+      {/* Debug indicator */}
+      <div className="fixed top-4 left-4 bg-green-600 text-white px-3 py-1 rounded text-sm z-50">
+        Index Loaded ✅
+      </div>
+
+      {/* Navigation */}
       <Navbar />
+
+      {/* Hero Section */}
       <div id="home">
         <Hero />
       </div>
+
+      {/* Services Section */}
       <div id="services">
         <Services />
       </div>
+
+      {/* Use Cases Section */}
       <div id="use-cases">
         <UseCases />
       </div>
+
+      {/* About Section */}
       <div id="about">
         <About />
       </div>
-      
-      {/* Vapi widget is now loaded directly in index.html */}
-      {/* No additional code needed here */}
+
+      {/* Vapi widget loads separately in index.html */}
     </div>
   );
 };
